fd5a2ede5c0c078746efe6d348358584c32a05f6
Adell Fael <new19.adel@gmail.com>
2023-09-28 02:16:05 +0300

    upgrading the theme selector

--- app/future/[day]/SpecificDayZaffat.tsx
+++ app/future/[day]/SpecificDayZaffat.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { SingleEntry } from "@/app/components/SingleEntry";
-import moment from "moment";
 import { SingleZaffe } from "@/app/types/SingleZaffe";
+import moment from "moment";
 import { useParams } from "next/navigation";
-
 
 type SpecificDayZaffatProps = {
   zaffat: SingleZaffe[];
@@ -15,7 +14,9 @@ export const SpecificDayZaffat: React.FC<SpecificDayZaffatProps> = ({
 }) => {
   const { day } = useParams();
 
-  const specificDayZaffat = zaffat.filter((z) => moment(z.date,"DD/MM/YYYY").format('DD-MM-YYYY') === day);
+  const specificDayZaffat = zaffat.filter(
+    (z) => moment(z.date, "DD/MM/YYYY").format("DD-MM-YYYY") === day,
+  );
 
   return (
     <>
