4435a7ecc50db813aa00dc3840e9e4d2d605bc4e
Adell Fael <new19.adel@gmail.com>
2023-09-28 22:36:39 +0300

    new font for ara and eng

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
 import { TabBar } from "./components/TabBar";
 import { ThemeChanger } from "./components/ThemeChanger";
 import "./globals.css";
 
-//👇 Import our second font
-import { IBM_Plex_Sans, Open_Sans } from "next/font/google";
-import { Footer } from "./components/Footer";
+import { Rubik } from "next/font/google";
+import { PortfolioLink } from "./components/PortfolioLink";
 
-const openSans = Open_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  //👇 Add variable to our object
-  variable: "--font-opensans",
+
+
+const rubik = Rubik({
+  weight: ["300","400","500","600","700"],
+  subsets: ["arabic"],
 });
 
-//👇 Configure the object for our second font
-const plexSans = IBM_Plex_Sans({
-  weight: "500",
-  subsets: ["latin"],
-  variable: "--font-ibm-plex-sans",
-});
 
 export const metadata: Metadata = {
   title: "Yalla Dance Group",
@@ -34,20 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${openSans.variable} ${plexSans.variable}`}>
-      <body>
-        
-      <Footer />
-
-      
-        <nav className="flex justify-center p-10 ">
+    <html lang="en">
+      <body className={rubik.className}>
+        <nav className="flex flex-col items-center justify-between gap-3 p-10 sm:flex-row ">
+          <PortfolioLink />
           <ThemeChanger />
         </nav>
 
         <TabBar />
 
         {children}
-
       </body>
     </html>
   );
